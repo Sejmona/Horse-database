42f0544226dc614a2838e4fce4133f1b0827ca88
Simona Šmelhausová <101262454+Sejmona@users.noreply.github.com>
2024-10-24 12:59:56 +0200

    add views for the forms

--- staj/staj_sprava/views.py
+++ staj/staj_sprava/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import HorseForm, CareForm, TrainingForm
 
-# Create your views here.
+def add_horse(request):
+    if request.method == 'POST':
+        form = HorseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('horse_list')  # Přesměrování na seznam koní po úspěšném přidání
+    else:
+        form = HorseForm()
+    return render(request, 'staj_sprava/add_horse.html', {'form': form})
+
+def add_care(request):
+    if request.method == 'POST':
+        form = CareForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('horse_list')  # Přesměrování na seznam koní po přidání péče
+    else:
+        form = CareForm()
+    return render(request, 'staj_sprava/add_care.html', {'form': form})
+
+def add_training(request):
+    if request.method == 'POST':
+        form = TrainingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('horse_list')  # Přesměrování na seznam koní po přidání tréninku
+    else:
+        form = TrainingForm()
+    return render(request, 'staj_sprava/add_training.html', {'form': form})
+
