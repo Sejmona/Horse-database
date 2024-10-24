9c8f1b4ac8e73b09b6a7ec71cf1055f4539c8845
Simona Šmelhausová <101262454+Sejmona@users.noreply.github.com>
2024-10-24 11:20:05 +0200

    Registrations models

--- staj/staj_sprava/admin.py
+++ staj/staj_sprava/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Horse, Care, Training
 
-# Register your models here.
+@admin.register(Horse)
+class HorseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'breed', 'age', 'health_status', 'last_vet_check')
+
+@admin.register(Care)
+class CareAdmin(admin.ModelAdmin):
+    list_display = ('horse', 'care_type', 'date')
+
+@admin.register(Training)
+class TrainingAdmin(admin.ModelAdmin):
+    list_display = ('horse', 'training_type', 'duration', 'date')
+
